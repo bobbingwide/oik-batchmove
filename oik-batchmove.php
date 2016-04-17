bbe7c5bfee8a9c70bf5abd6f9694d2c2a6ff2146
Herb Miller <herb@bobbingwide.com>
2016-04-17 14:51:24 +0100

    v2.4.2 - tested with WordPress 4.5

--- oik-batchmove.php
+++ oik-batchmove.php
@@ -3,13 +3,13 @@
 Plugin Name: oik batchmove
 Plugin URI: http://www.oik-plugins.com/oik-plugins/oik-batchmove
 Description: Batch change post categories or published date incl. CRON rescheduling
-Version: 2.4.1
+Version: 2.4.2
 Author: bobbingwide
 Author URI: http://www.oik-plugins.com/author/bobbingwide
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-    Copyright 2013-2015 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2013-2016 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
@@ -98,7 +98,7 @@ function oik_batchmove_admin_menu() {
 /**
  * Implement "admin_notices" action for oik-batchmove"
  * 
- * Now depends on oik v2.5 
+ * v2.4.2 now depends on oik v3.0.0 or higher 
  */ 
 function oik_batchmove_activation() {
   static $plugin_basename = null;
@@ -109,7 +109,7 @@ function oik_batchmove_activation() {
       require_once( "admin/oik-activation.php" );
     }
   }  
-  $depends = "oik:2.5";
+  $depends = "oik:3.0.0";
   oik_plugin_lazy_activation( __FILE__, $depends, "oik_plugin_plugin_inactive" );
 }
 
