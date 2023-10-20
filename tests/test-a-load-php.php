7be24ffa196f5b61c129be3bd1bafecd69ea92f2
Herb Miller <herb@bobbingwide.com>
2023-10-20 14:59:49 +0100

    Add PHPUnit tests loading all PHP files #5

---
+++ tests/test-a-load-php.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package oik-batchmove
+ * @copyright (C) Copyright Bobbing Wide 2023
+ *
+ * Unit tests to load all the PHP files for PHP 8.2
+ */
+class Tests_load_php extends BW_UnitTestCase
+{
+
+	/**
+	 * set up logic
+	 *
+	 * - ensure any database updates are rolled back
+	 * - we need oik-googlemap to load the functions we're testing
+	 */
+	function setUp(): void 	{
+		parent::setUp();
+	}
+
+	function test_load_admin_php() {
+		oik_require( 'admin/oik-activation.php', 'oik-batchmove');
+		oik_require( 'admin/oik-batchmove.php', 'oik-batchmove');
+		oik_require( 'admin/oik-batchmove-categories.php', 'oik-batchmove');
+		oik_require( 'admin/oik-batchmove-cron.php', 'oik-batchmove');
+		oik_require( 'admin/oik-batchmove-tags.php', 'oik-batchmove');
+		$this->assertTrue( true );
+	}
+	
+	function test_load_plugin_php() {
+		oik_require( 'oik-batchmove.php', 'oik-batchmove');
+		$this->assertTrue( true );
+	}
+}
+
+
+
